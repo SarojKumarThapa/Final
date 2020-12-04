7ae44ea162dadbf51e0471772ebb32c01428180e
saroj <st4724011@gmail.com>
2020-12-04 20:51:50 +0545

    Some designes changed and some labels are being added in the application

--- Final/MainForm.Designer.cs
+++ Final/MainForm.Designer.cs
@@ -32,11 +32,14 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.applicationGuideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.txt_Command_Box = new System.Windows.Forms.TextBox();
             this.txt_Execution_Box = new System.Windows.Forms.TextBox();
             this.Output_Box = new System.Windows.Forms.Panel();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,8 +51,8 @@
             this.applicationGuideToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(629, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(892, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,80 +63,113 @@
             this.importToolStripMenuItem,
             this.closeToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // importToolStripMenuItem
             // 
             this.importToolStripMenuItem.Name = "importToolStripMenuItem";
-            this.importToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.importToolStripMenuItem.Text = "Import";
             this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
             // applicationGuideToolStripMenuItem
             // 
             this.applicationGuideToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Highlight;
             this.applicationGuideToolStripMenuItem.Name = "applicationGuideToolStripMenuItem";
-            this.applicationGuideToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.applicationGuideToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.applicationGuideToolStripMenuItem.Text = "Help";
             this.applicationGuideToolStripMenuItem.Click += new System.EventHandler(this.applicationGuideToolStripMenuItem_Click);
             // 
             // txt_Command_Box
             // 
             this.txt_Command_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Command_Box.Location = new System.Drawing.Point(28, 42);
-            this.txt_Command_Box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txt_Command_Box.MaximumSize = new System.Drawing.Size(80, 57);
-            this.txt_Command_Box.MinimumSize = new System.Drawing.Size(507, 81);
+            this.txt_Command_Box.Location = new System.Drawing.Point(500, 78);
+            this.txt_Command_Box.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_Command_Box.MaximumSize = new System.Drawing.Size(61, 47);
+            this.txt_Command_Box.MinimumSize = new System.Drawing.Size(381, 67);
             this.txt_Command_Box.Multiline = true;
             this.txt_Command_Box.Name = "txt_Command_Box";
-            this.txt_Command_Box.Size = new System.Drawing.Size(507, 81);
+            this.txt_Command_Box.Size = new System.Drawing.Size(381, 67);
             this.txt_Command_Box.TabIndex = 12;
             // 
             // txt_Execution_Box
             // 
             this.txt_Execution_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Execution_Box.Location = new System.Drawing.Point(28, 126);
-            this.txt_Execution_Box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Execution_Box.Location = new System.Drawing.Point(657, 226);
+            this.txt_Execution_Box.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Execution_Box.Name = "txt_Execution_Box";
-            this.txt_Execution_Box.Size = new System.Drawing.Size(144, 41);
+            this.txt_Execution_Box.Size = new System.Drawing.Size(129, 35);
             this.txt_Execution_Box.TabIndex = 3;
             this.txt_Execution_Box.TextChanged += new System.EventHandler(this.txt_Execution_Box_TextChanged);
             // 
             // Output_Box
             // 
-            this.Output_Box.Location = new System.Drawing.Point(28, 169);
-            this.Output_Box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Output_Box.Location = new System.Drawing.Point(11, 78);
+            this.Output_Box.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Output_Box.Name = "Output_Box";
-            this.Output_Box.Size = new System.Drawing.Size(569, 294);
+            this.Output_Box.Size = new System.Drawing.Size(458, 319);
             this.Output_Box.TabIndex = 6;
             // 
-            // closeToolStripMenuItem
+            // label1
             // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.closeToolStripMenuItem.Text = "Close";
-            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label1.Location = new System.Drawing.Point(122, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 31);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Result Box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(550, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 31);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Commands and Shapes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label3.Location = new System.Drawing.Point(573, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 31);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Execution Instruction";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 481);
+            this.ClientSize = new System.Drawing.Size(892, 477);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Output_Box);
             this.Controls.Add(this.txt_Execution_Box);
             this.Controls.Add(this.txt_Command_Box);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "Main Form";
             this.menuStrip1.ResumeLayout(false);
@@ -154,6 +190,9 @@
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
